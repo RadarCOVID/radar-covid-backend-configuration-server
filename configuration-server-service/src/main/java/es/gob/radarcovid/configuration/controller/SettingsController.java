f83553bac9fd2c30ac44a32aa9e70c29523e4f4c
mapalomar <mapalomar@minsait.com>
2020-10-07 19:35:20 +0200

    [MAPR] Modified exception handling. Error messages has been included.

--- configuration-server-service/src/main/java/es/gob/radarcovid/configuration/controller/SettingsController.java
+++ configuration-server-service/src/main/java/es/gob/radarcovid/configuration/controller/SettingsController.java
@@ -9,7 +9,6 @@
  */
 package es.gob.radarcovid.configuration.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.gob.radarcovid.common.annotation.Loggable;
+import es.gob.radarcovid.configuration.api.ErrorDto;
 import es.gob.radarcovid.configuration.api.SettingsDto;
 import es.gob.radarcovid.configuration.business.SettingsService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,10 +25,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/settings")
 @RequiredArgsConstructor
+@Slf4j
 public class SettingsController {
 
     private final SettingsService service;
@@ -38,19 +41,15 @@ public class SettingsController {
     @Operation(summary = "Get application settings", description = "Get application settings", tags = { "settings" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = SettingsDto.class))),
-        @ApiResponse(responseCode = "400", description = "error", content = @Content(schema = @Schema(implementation = Void.class))),
-        @ApiResponse(responseCode = "500", description = "error", content = @Content(schema = @Schema(implementation = Void.class)))
+        @ApiResponse(responseCode = "400", description = "error", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+        @ApiResponse(responseCode = "404", description = "error", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+        @ApiResponse(responseCode = "500", description = "error", content = @Content(schema = @Schema(implementation = ErrorDto.class)))
     })	
     public ResponseEntity<SettingsDto> getSettings() {
-        
-    	HttpStatus httpStatus;
-    	SettingsDto settingsDto = null;
-        try {
-        	settingsDto = service.getSettings();
-            httpStatus = HttpStatus.OK;
-        } catch (Exception e) {
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-        return ResponseEntity.status(httpStatus).cacheControl(CacheControl.noStore()).body(settingsDto);
+       
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.cacheControl(CacheControl.noStore())
+				.body(this.service.getSettings());
     }
 }
